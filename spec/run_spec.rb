c2bd173b7de5e4cc50a22e1c126b566856732cf7
nf2000 <nimraf@chillibean.com>
2021-09-15 10:17:30 +0100

    fixed up indentation

--- spec/run_spec.rb
+++ spec/run_spec.rb
@@ -34,26 +34,25 @@ describe 'Names Challenge' do
 
   end
   context '#random_team_split' do
-  it 'returns a multidimensional array - one array that contains two arrays in this case.' do 
+    it 'returns a multidimensional array - one array that contains two arrays in this case.' do 
     
-    r_split = random_team_split(NAMES)
-    #needs to be 2 arrays in an array 
-    puts "#{r_split[0] && r_split[1]}"
+      r_split = random_team_split(NAMES)
+      #needs to be 2 arrays in an array 
+      puts "#{r_split[0] && r_split[1]}"
       expect(random_team_split(NAMES)).to be_an(Array)
       expect(r_split.length()).to eq(2) # checks if the length of the array is 2 
 
-      
+        
       # run 2 times to see if the element is an array and the length of each array is 8
 
       2.times do |iterator|
         expect(r_split[iterator]).to be_an(Array)
-        #puts r_split[iterator]
         expect(r_split[iterator].length()).to eq(8)
       end
-        #expect that both arrays within multi_d_array are random
-       flat_array = r_split.flatten
-       expect(flat_array).not_to eq(NAMES)
-       expect(r_split[0]).not_to eq(r_split[1])
+          #expect that both arrays within multi_d_array are random
+      flat_array = r_split.flatten
+      expect(flat_array).not_to eq(NAMES)
+      expect(r_split[0]).not_to eq(r_split[1])
     end
   end
 
