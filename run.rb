1964bbfe90507e994564cb2d319cd3474a78ca86
nf2000 <nimraf@chillibean.com>
2021-09-15 10:16:36 +0100

    1) Fixed up indentation 2) changed the while loop into a times loop 3) commented out the puts lines at end of program because it was interferring with rspec test

--- run.rb
+++ run.rb
@@ -25,21 +25,17 @@ def names_list(names)
   # returns a string with each name in 'names' with a comma between them
   
     return "#{names.join(",")}"
-
-
 end
 
 def names_list_alphabetical(names)
   # returns a string with each name in 'names' with a comma between them in alphabetical order
   sortedarray = names.sort
-  return "#{sortedarray.join(",")}"
-
-    
+  return "#{sortedarray.join(",")}"   
 end
+
 
 def random_name(names)
   # returns one of the names at random
-
   randomno = rand(names.count)  # generating and storing random number
   return randomname = names[randomno] # returing the name at that random generated index
 
@@ -48,24 +44,23 @@ end
 def random_team_split(names)
   
   count = 0 
-  newarray = [],[]
-  while count < names.count
-    randomname = names.sample # suffles the array and outputs one element 
+  newarray = [[],[]]
+
+  8.times do |iterator|
+    randomname = names.sample # sample takes random elemnet from array 
     newarray[0] << (randomname)
     names.delete(randomname)
-
-    count += 1
   end
    
   names.each do |element|
     newarray[1] << element
   end
 
-return newarray
-
+  return newarray
 
 end
-array = names_array(NAMES)
+
+=begin
 puts " ------ Name List ---- "
 puts " \n" + "#{names_list(NAMES)}"
 puts " \n" + "----- Alphabetically sorted -----"
@@ -74,5 +69,5 @@ puts " \n" + " ------ Random name ------"
 puts " \n" + "#{random_name(NAMES)}"
 puts "\n" + "------------ Team split ---------------"
 puts "\n" + "#{random_team_split(NAMES)}"
-
+=end
 
